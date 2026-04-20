daad882932e23b0b811ca9b6dbf0926e7d59298f
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-04-19 22:14:08 -0600

    Add InjectionEngine class for advanced injection

---
+++ core/engine.py
@@ -0,0 +1,7 @@
+# Motor de inyección avanzado (próximamente)
+class InjectionEngine:
+    def __init__(self):
+        pass
+    
+    def run(self):
+        print("[*] Motor avanzado de MapSQL - En desarrollo")
